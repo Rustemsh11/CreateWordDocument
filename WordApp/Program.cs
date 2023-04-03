eba96af71587ad630210b75fb9cb95960345c882
Rustem Shaiheraziev <mr.shaykheraziev@mail.ru>
2023-04-03 15:12:59 +0300

    Add first commit

---
+++ WordApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WordApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordDocument word = new WordDocument();
+            word.SuplyerName = "supl";
+            word.SuplyerINN = "23456789";
+            word.SuplyerKPP = "2345678908765432";
+            word.CompanyName = "gazStroyServis";
+            word.CompanyINN = "123456543";
+            word.CompanyKPP = "234532";
+            word.SnabName = "Igor";
+            word.SnabPhone = "89600655315";
+            word.Email = "mr.shaykheraziev@mail.ru";
+            word.Create();
+         
+        }
+    }
+}
---
+++ WordApp/WordDocument.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.Office.Interop.Word;
+
+namespace WordApp
+{
+    public class WordDocument
+    {
+        public string SuplyerName { get; set; }
+        public string SuplyerINN { get; set; }
+        public string SuplyerKPP { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyINN { get; set; }
+        public string CompanyKPP { get; set; }
+        public string SnabName { get; set; }
+        public string SnabPhone { get; set; }
+        public string Email { get; set; }
+
+
+        public void Create()
+        {
+            Application application = new Application();
+            Document document = application.Documents.Add(Visible: true);
+            Paragraph paragraph = document.Paragraphs.Add();
+            document.Paragraphs.Format.Alignment = WdParagraphAlignment.wdAlignParagraphRight;
+            document.Paragraphs.Format.LineSpacingRule = WdLineSpacing.wdLineSpaceMultiple;
+
+            Range range = document.Paragraphs[1].Range;
+            range.Text = string.Format("Кому: {0}\vИнн: {1} КПП: {2}\v\v От: {3}\vИнн: {4} КПП: {5}\vКонтактное лицо: {6}\vТелефон: {7}\v Email: {8}"
+                , SuplyerName, SuplyerINN, SuplyerKPP, CompanyName, CompanyINN, CompanyKPP, SnabName, SnabPhone,Email);
+            range.Font.Name = "Times New Roman";
+            range.Font.Size = 14;
+
+            Paragraph paragraph1 = document.Paragraphs.Add();
+            document.Paragraphs[2].Format.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+            Range range2 = document.Paragraphs[2].Range;
+            range2.Text = "Запрос о предоставлении ценовой информации";
+            range2.Font.Name = "Times New Roman";
+            range2.Font.Size = 20;
+            range2.Font.Bold = 0;
+            document.SaveAs(@"C:\Users\агроном\Desktop\test.docx");
+            document.Close();
+
+            application.Quit();
+        }
+    }
+}
